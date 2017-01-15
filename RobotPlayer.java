2a6e6341269c7dcbd72b370f5171dfc03fcc220d
kershockr <tim.merino@live.com>
2017-01-14 21:36:15 -0500

    Basic scout functionality
    Added channels for scouts
    Scouts scan nearby neutral trees and shake them
    If none are found, scouts move randomly
    Moved treeinfo scanning from gardener class to seperate methods

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -8,9 +8,12 @@ public strictfp class RobotPlayer {
     static final int HEAD_ARCHON_CHANNEL = 1; //stores the ID of the head archon
     static final int GARDENER_COUNT_CHANNEL = 2; //the final count of gardeners read by archons
     static final int GARDENER_SUM_CHANNEL = 3; //the sum of gardeners incremented by gardeners on their turn
+    static final int SCOUT_COUNT_CHANNEL = 4;
+    static final int SCOUT_SUM_CHANNEL = 5;
 
     static final int MAX_GARDENERS = 5; //max number of gardeners we want to build
-    static final int MAX_TREES = 6; //max number of trees
+    static final int MAX_TREES = 8; //max number of trees
+    static final int MAX_SCOUTS = 3;
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
@@ -59,7 +62,14 @@ public strictfp class RobotPlayer {
                 if(rc.getID() == rc.readBroadcast(HEAD_ARCHON_CHANNEL)) { // only the head archon does the counting
                     rc.broadcast(GARDENER_COUNT_CHANNEL, rc.readBroadcast(GARDENER_SUM_CHANNEL)); //moves the sum to the final count channel to be used by archons
                     rc.broadcast(GARDENER_SUM_CHANNEL, 0); //resets the sum channel
+                    rc.broadcast(SCOUT_COUNT_CHANNEL, rc.readBroadcast(SCOUT_SUM_CHANNEL));
+                    rc.broadcast(SCOUT_SUM_CHANNEL, 0);
+                    if(rc.getTeamBullets() > 500)
+                    {
+                        rc.donate(10);
+                    }
                 }
+
                 Direction dir = randomDirection();
                 if (rc.canHireGardener(dir) && rc.readBroadcast(GARDENER_COUNT_CHANNEL) < MAX_GARDENERS) { //will hire a gardener if it is possible and there are less than the desired maximum
                     rc.hireGardener(dir);
@@ -77,32 +87,22 @@ public strictfp class RobotPlayer {
     static void runGardener() throws GameActionException {
         while (true) {
             try {
-                TreeInfo[] nearbyTrees = rc.senseNearbyTrees(7, rc.getTeam()); //get an array of nearby trees
                 rc.broadcast(GARDENER_SUM_CHANNEL, rc.readBroadcast(GARDENER_SUM_CHANNEL) + 1); //adds 1 to the sum channel
                 Direction dir = randomDirection();
 
                 //this block will execute for watering nearby trees
-                if(nearbyTrees.length != 0) { //will only execute if there are nearby trees
-                    int dyingTreeID = 0; //id that will be set to the value of a tree that needs help
-                    MapLocation dyingTreeLoc = null; //Maplocation of the dying tree
-                    for (int i = 0; i < nearbyTrees.length; i++) { //for loop to parse through the nearby trees
-                        if(nearbyTrees[i].getMaxHealth() - nearbyTrees[i].getHealth() >= 5){ //watering heals for 5, so if a tree is missing 5 or more health, we call them a dying tree and try to help them
-                            dyingTreeID = nearbyTrees[i].getID(); //sets the ID of the tree with missing health
-                            dyingTreeLoc = nearbyTrees[i].getLocation(); //same for location
-                            break;
-                        }
+                TreeInfo dyingTree = getDyingTree();
+                if (dyingTree != null) {
+                    if (rc.canWater(dyingTree.getID())) { //tries to water the dying tree
+                        rc.water(dyingTree.getID());
+                    } else if (rc.canMove(dyingTree.getLocation())) { //if we have a dying tree, and we can move towards them, we do
+                        rc.move(dyingTree.getLocation());
                     }
-                    if(rc.canWater(dyingTreeID)){ //tries to water the dying tree
-                        rc.water(dyingTreeID);
-                    }
-                    else if (dyingTreeLoc != null && rc.canMove(dyingTreeLoc)) { //if we have a dying tree, and we can move towards them, we do
-                        rc.move(dyingTreeLoc);
-                    }
-
-
                 }
-                if(rc.getTreeCount() < MAX_TREES && rc.canPlantTree(dir)){ //if there aren't enough trees and a tree can be planted in the random direciton dir
+                if (rc.getTreeCount() < MAX_TREES && rc.canPlantTree(dir)) { //if there aren't enough trees and a tree can be planted in the random direciton dir
                     rc.plantTree(dir);
+                } else if(rc.getTeamBullets() > 80 && rc.canBuildRobot(RobotType.SCOUT, dir) && rc.readBroadcast(SCOUT_COUNT_CHANNEL) < MAX_SCOUTS){
+                    rc.buildRobot(RobotType.SCOUT, dir);
                 }
                 else { //contained in an else because we don't want gardeners wandering away from their trees
                     move(dir);
@@ -151,6 +151,19 @@ public strictfp class RobotPlayer {
     static void runScout() throws GameActionException {
         while (true) {
             try {
+                rc.broadcast(SCOUT_SUM_CHANNEL, rc.readBroadcast(SCOUT_SUM_CHANNEL) + 1); //counting number of scouts
+                TreeInfo neutralTree = getNearbyTree(Team.NEUTRAL);  //get the closest nearby tree
+                if(neutralTree != null && neutralTree.getContainedBullets() != 0){ //if one exists and it has bullets in it
+                    if(rc.canShake(neutralTree.getID())){ //if we can shake it
+                        rc.shake(neutralTree.getID());
+                    }
+                    else {
+                        moveTo(neutralTree.getLocation()); //else move towards it
+                    }
+                } else { //if there is nearby tree or the closest one has no bullets, move randomly
+                    Direction dir = randomDirection();
+                    move(dir);
+                }
                 Clock.yield();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -168,7 +181,37 @@ public strictfp class RobotPlayer {
         }
     }
 
+    static void moveTo(MapLocation loc) throws GameActionException{
+        try{
+            if(rc.canMove(loc)){
+                rc.move(loc);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public static Direction randomDirection() {
         return new Direction(rand.nextFloat() * 2 * (float) Math.PI); //takes a random number from "rand" multiplies it by 2pi to get a random radian
+    }
+
+    public static TreeInfo getDyingTree(){ //will return treeinfo of a dying tree (missing 10 more more health) or null if there are none
+        TreeInfo[] nearbyTrees = rc.senseNearbyTrees(7, rc.getTeam()); //get an array of nearby trees
+        if(nearbyTrees.length != 0) { //will only execute if there are nearby trees
+            for (int i = 0; i < nearbyTrees.length; i++) { //for loop to parse through the nearby trees
+                if(nearbyTrees[i].getMaxHealth() - nearbyTrees[i].getHealth() > 10){
+                    return nearbyTrees[i];
+                }
+                }
+            }
+        return null;
+    }
+
+    public static TreeInfo getNearbyTree(Team team){ //will return treeinfo of a nearby tree (missing 10 more more health) or null if there are none
+        TreeInfo[] nearbyTrees = rc.senseNearbyTrees(10, team); //get an array of nearby trees
+        if(nearbyTrees.length != 0) {
+            return nearbyTrees[0];
+        }
+        return null;
     }
 }
